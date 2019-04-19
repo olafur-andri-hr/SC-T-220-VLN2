a5b0bb58d2c0e4074644d725d3bd97b83b87bf41
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-04-19 01:15:53 +0000

    Update settings.py

--- VLN2/settings.py
+++ VLN2/settings.py
@@ -132,5 +132,5 @@ STATIC_URL = '/static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
+print(list(os.environ.keys()))
 django_heroku.settings(locals())
