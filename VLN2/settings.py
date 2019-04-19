ecdc23018963342081d841b6ded469370dd80b0d
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-04-19 01:14:46 +0000

    settings, configuration

--- VLN2/settings.py
+++ VLN2/settings.py
@@ -81,14 +81,14 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'Django',
-    #     'USER': 'django',
-    #     'PASSWORD': 'django_password',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Django',
+        'USER': 'django',
+        'PASSWORD': 'django_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 
