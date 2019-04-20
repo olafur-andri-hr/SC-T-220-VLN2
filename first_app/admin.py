ece763af7e940540b687f5c2f76c2ccb89632626
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-04-20 20:47:09 +0000

    revert manual user add

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-
-from django.contrib.auth.models import User
-u = User.objects.create_superuser("gudni", "email@example.com", "temp")
