bcd7619246a681b8b0cfe1f88215624856234bdb
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-04-20 13:48:46 +0000

    Update admin.py

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib.auth.models import User
+u = User.objects.create_superuser("gudni", "email@example.com", "temp")
