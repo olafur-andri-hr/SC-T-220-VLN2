89d6ebef70fcea0f2f1c3fbccca2bc0222a0db13
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-04-20 13:40:36 +0000

    migrations on build

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate --noinput
 web: gunicorn VLN2.wsgi --log-file -
--- VLN2/settings.py
+++ VLN2/settings.py
@@ -135,5 +135,3 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 django_heroku.settings(locals())
-
-print(DATABASES['default'])
