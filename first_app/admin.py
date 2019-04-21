5a31cf09a604aaa6b40737b73d7c348d5c169932
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-04-21 21:58:34 +0000

    register demo models to admin page

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Color, Vehicle, Car, Scooter
+
+admin.site.register(Color)
+admin.site.register(Vehicle)
+admin.site.register(Car)
+admin.site.register(Scooter)
