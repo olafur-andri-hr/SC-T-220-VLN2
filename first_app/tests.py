6db1000a60ba2ddf2e4a2f4c6e3b75772f856c8f
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-04-22 00:17:16 +0000

    more detailed tests

--- first_app/tests.py
+++ first_app/tests.py
@@ -1,8 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Color, Vehicle, Car, Scooter
 
 
 class MyTest(TestCase):
-    def test_test(self):
-        self.assertEquals(1, 1)
+    def setUp(self):
+        self.test_color = Color.objects.create(color="black")
+        Car.objects.create(license_plate="CK565", wheel_count=4,
+                           color=self.test_color, manufacturer="Volvo")
+
+    def test_color(self):
+        color = Color.objects.all()[0]  # Get first color
+        self.assertEqual(color.color, "Black")
+        self.assertEquals(str(self.test_color), "Black")
+
+    def test_car(self):
+        car = Car.objects.get(license_plate="CK565")
+        self.assertEquals(car.manufacturer, "Volvo")
+        self.assertEquals(car.color, self.test_color)
