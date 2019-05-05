e35bf8d56229435bc4c85c0cb093b7567d737704
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-05-05 16:43:43 +0000

    get postal code info from country and postal code
    GET /location/<country_code>/<postal_code>

--- location/urls.py
+++ location/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<country_code>/<postal_code>', views.get_town, name='get town'),
     path('', views.index, name='index'),
 ]
