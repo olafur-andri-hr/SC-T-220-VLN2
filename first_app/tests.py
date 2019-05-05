a38b034b9c2e5944a87755c452f0017bc0c323de
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-05-05 12:51:41 +0000

    remove old tests

--- first_app/tests.py
+++ first_app/tests.py
@@ -4,18 +4,18 @@ from django.test import TestCase
 from .models import Color, Vehicle, Car, Scooter
 
 
-class MyTest(TestCase):
-    def setUp(self):
-        self.test_color = Color.objects.create(color="black")
-        Car.objects.create(license_plate="CK565", wheel_count=4,
-                           color=self.test_color, manufacturer="Volvo")
+# class MyTest(TestCase):
+#     def setUp(self):
+#         self.test_color = Color.objects.create(color="black")
+#         Car.objects.create(license_plate="CK565", wheel_count=4,
+#                            color=self.test_color, manufacturer="Volvo")
 
-    def test_color(self):
-        color = Color.objects.all()[0]  # Get first color
-        self.assertEqual(color.color, "Black")
-        self.assertEquals(str(self.test_color), "Black")
+#     def test_color(self):
+#         color = Color.objects.all()[0]  # Get first color
+#         self.assertEqual(color.color, "Black")
+#         self.assertEquals(str(self.test_color), "Black")
 
-    def test_car(self):
-        car = Car.objects.get(license_plate="CK565")
-        self.assertEquals(car.manufacturer, "Volvo")
-        self.assertEquals(car.color, self.test_color)
+#     def test_car(self):
+#         car = Car.objects.get(license_plate="CK565")
+#         self.assertEquals(car.manufacturer, "Volvo")
+#         self.assertEquals(car.color, self.test_color)
