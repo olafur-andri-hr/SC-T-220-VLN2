33694a7a95144d090ce0c0268a4dfb8f15b38e32
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-05-06 12:49:27 +0000

    fixed error with urls in castleapartments

--- castleapartments/urls.py
+++ castleapartments/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about')
-    path('login/', views.login, name='login')
-    path('signup/', views.signup, name='signup')
-    path('sell/', views.sell, name='sell')
+    path('about/', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('sell/', views.sell, name='sell'),
 ]
