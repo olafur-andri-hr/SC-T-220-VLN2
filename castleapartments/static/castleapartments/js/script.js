6c1b120aaecbb13acee3cf08bfb8abc43ab9e570
olafur-andri-hr <olafurd18@ru.is>
2019-05-07 15:03:08 +0000

    Added a hint that users can see to know that the listing can be seen by scrolling down

--- castleapartments/static/castleapartments/js/script.js
+++ castleapartments/static/castleapartments/js/script.js
@@ -1,9 +1,39 @@
+// Global variables
+const listingHint = document.getElementById('listing-hint');
+let hintIsHidden = false;
+
 /**
  * The main function for the file
  */
 function main() {
-  const message = 'testing';
-  console.log(message);
+  addEventListeners();
+}
+
+/**
+ * Adds event listeners to all the necessary HTML elements
+ */
+function addEventListeners() {
+  window.addEventListener('scroll', onScroll);
+}
+
+/**
+ * The window scroll event handler
+ * @param {Event} e The object that represents the scroll event
+ */
+function onScroll(e) {
+  if (window.pageYOffset > 10) {
+    if (hintIsHidden) {
+      return;
+    }
+    hintIsHidden = true;
+    listingHint.className = 'hide';
+  } else {
+    if (!hintIsHidden) {
+      return;
+    }
+    listingHint.className = '';
+    hintIsHidden = false;
+  }
 }
 
 main();
