c90d238d28216262333602687b7a05fcaa3fb784
thorbreki1199 <40344038+thorbreki@users.noreply.github.com>
2019-05-07 13:21:30 +0000

    haha

--- base/static/js/script.js
+++ base/static/js/script.js
@@ -1,5 +1,17 @@
+document.getElementById("dropdownnav").addEventListener("click", hamburger);
+document.getElementById("sidebar").addEventListener("click", sidebardropdown);
+
 function sidebardropdown() {
   document.getElementById("sidebar").classList.toggle("active");
   console.log("Hi");
 }
 
+function hamburger() {
+  navbar = document.getElementById("dropdownnav");
+  if (navbar.className === "active") {
+    navbar.className = "notActive";
+  }
+  else {
+    navbar.className = "active";
+  }
+}
