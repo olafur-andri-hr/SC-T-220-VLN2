216babd99fa5a172d3087176266beb798e7baaeb
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-05-07 17:17:27 +0000

    sidebar updated

--- base/static/js/script.js
+++ base/static/js/script.js
@@ -1,6 +1,6 @@
 document.getElementById('hamburger').addEventListener('click', hamburger);
-document.getElementById('accountdiv')
-    .addEventListener('click', sidebardropdown);
+// document.getElementById('accountdiv')
+//     .addEventListener('click', sidebardropdown);
 
 /**
  * function for sliding the sidebar nav menu
@@ -13,10 +13,26 @@ function sidebardropdown() {
  * function for the hamburger menu
  */
 function hamburger() {
-  navbar = document.getElementById('dropdownnav');
+  const navbar = document.getElementById('dropdownnav');
   if (navbar.className === 'active') {
     navbar.className = 'notActive';
   } else {
     navbar.className = 'active';
   }
 }
+
+window.addEventListener('click', function(event) {
+  let target = event.target;
+  const overlay = document.getElementById('sidebar');
+  const div = document.getElementById('accountdiv');
+  while (target != document.body) {
+    if (target == div) {
+      overlay.classList.toggle('active');
+      return;
+    } else if (target == overlay) {
+      return;
+    }
+    target = target.parentNode;
+  }
+  overlay.classList.remove('active');
+});
