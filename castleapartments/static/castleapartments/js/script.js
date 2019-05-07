8a0f91723d6350d6de1eac8a7c59279f2f131222
olafur-andri-hr <olafurd18@ru.is>
2019-05-07 16:31:02 +0000

    Users can now use the 'scroll down to view listings' button to actually do that

--- castleapartments/static/castleapartments/js/script.js
+++ castleapartments/static/castleapartments/js/script.js
@@ -1,5 +1,6 @@
 // Global variables
 const listingHint = document.getElementById('listing-hint');
+const siteHeader = document.getElementById('site_header');
 let hintIsHidden = false;
 
 /**
@@ -13,7 +14,11 @@ function main() {
  * Adds event listeners to all the necessary HTML elements
  */
 function addEventListeners() {
+  const editSearchLink = document.getElementById('edit_search');
+
   window.addEventListener('scroll', onScroll);
+  editSearchLink.addEventListener('click', scrollToTop);
+  listingHint.addEventListener('click', scrollToListings);
 }
 
 /**
@@ -36,6 +41,44 @@ function onScroll(e) {
     }
     listingHint.className = '';
     hintIsHidden = false;
+  }
+}
+
+/**
+ * Scrolls the user to the top of the website in a smoooth manner
+ * @param {Event} e The event object for this function
+ */
+function scrollToTop(e) {
+  try {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+    e.preventDefault();
+  } catch (_) {
+    // Do nothing
+  }
+}
+
+/**
+ * Scrolls the user in a smooth manner down to the recent listings page
+ * @param {Event} e The event object for this function
+ */
+function scrollToListings(e) {
+  try {
+    const header = document.getElementById('search_results_header');
+    const headerHeight = siteHeader.getBoundingClientRect().height;
+    const headerOffsetTop = header.getBoundingClientRect().top;
+    const offsetTop = headerOffsetTop - headerHeight;
+
+    window.scroll({
+      top: offsetTop,
+      left: 0,
+      behavior: 'smooth',
+    });
+  } catch (_) {
+    // Do nothing
   }
 }
 
