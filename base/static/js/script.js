85d7202502bd98e4492927a9ca0fb1dde89b564d
Pálmi C Rúnarsson <pancakeprins@gmail.com>
2019-05-07 13:02:46 +0000

    sidebar js

--- base/static/js/script.js
+++ base/static/js/script.js
@@ -1,16 +1,5 @@
-function sidebar() {
-    document.getElementById("sidebar").classList.toggle("active");
-  }
+function sidebardropdown() {
+  document.getElementById("sidebar").classList.toggle("active");
+  console.log("Hi");
+}
 
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
