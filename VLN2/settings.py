0e4fcd2510b6ca02e83a708e1118ca3b8012ae90
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-05-07 10:55:48 +0000

    register userinfo to admin site

--- VLN2/settings.py
+++ VLN2/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
