3185f30183efec8217133ad04aa2027ecfbb9f77
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-05-09 14:07:16 +0000

    hidden fields in search form

--- apartments/views.py
+++ apartments/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Listing
+from castleapartments.forms import SearchForm
 
 # Create your views here.
 
@@ -8,3 +9,12 @@ from .models import Listing
 def listing(request, listing_id):
     listing = Listing.objects.get(uuid=listing_id)
     return HttpResponse(f"This is the listing for {listing.apartment.address}")
+
+
+def search(request):
+    if request.method == "POST":
+        search_form = SearchForm(request.POST)
+        if search_form.is_valid():
+            parameters = search_form.cleaned_data
+            results = Listing.objects.filter(parameters)
+            return JsonResponse(results)
