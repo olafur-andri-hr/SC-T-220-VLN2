a6bb446e4b1cfbb2c02169498c50881c7aa82d19
olafur-andri-hr <olafurd18@ru.is>
2019-05-10 10:05:23 +0000

    Added search history feature

--- apartments/urls.py
+++ apartments/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('<uuid:listing_id>/', views.listing),
-    path('api/search/', views.search)
+    path('api/search/', views.search),
+    path('api/search/<str:listing_ids>/', views.get_many_by_id)
 ]
--- apartments/views.py
+++ apartments/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.core import serializers
 from .models import Listing
 from castleapartments.forms import SearchForm
 
@@ -18,3 +19,25 @@ def search(request):
             parameters = search_form.cleaned_data
             results = Listing.objects.filter(parameters)
             return JsonResponse(results)
+
+
+def get_many_by_id(request, listing_ids):
+    id_list = listing_ids.split(",")
+    data = []
+    for listing_id in id_list:
+        listing = Listing.objects.get(uuid=listing_id)
+        data.append({
+            "image": listing.apartment.apartmentimage_set.first().image.url,
+            "listing_date": listing.listing_date,
+            "address": listing.apartment.address,
+            "zip_code": listing.apartment.postal_code.zip_code,
+            "town": listing.apartment.postal_code.town,
+            "country": listing.apartment.postal_code.country.name,
+            "apt_number": listing.apartment.apt_number,
+            "type": listing.apartment.apartment_type.name,
+            "num_rooms": listing.apartment.num_rooms,
+            "price": listing.apartment.appraisal,
+            "description": listing.apartment.description,
+            "uuid": listing_id,
+        })
+    return JsonResponse(data, safe=False)
