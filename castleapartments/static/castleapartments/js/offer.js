116fc7ebcca7218511edf06904044ca75bbddd77
thorbreki1199 <40344038+thorbreki@users.noreply.github.com>
2019-05-13 13:08:59 +0000

    lagaði javascript

--- castleapartments/static/castleapartments/js/offer.js
+++ castleapartments/static/castleapartments/js/offer.js
@@ -4,11 +4,20 @@ theButton.addEventListener('click', showConfirm);
 const offerAmount = document.getElementById('id_offer_amount');
 const conveyance = document.getElementById('id_conveyance_date');
 const creditCardNumber = document.getElementById('id_credit_card_number');
+const expiry = document.getElementById('id_credit_card_expiration_date');
 
 
 /**
  * This is a useless comment because I'm too lazy to do something with it
  */
 function showConfirm() {
+  const amounttd = document.getElementById('amounttd');
+  const conveyancetd = document.getElementById('conveyancetd');
+  const creditCardtd = document.getElementById('creditcardtd');
+  const ccExpirytd = document.getElementById('ccexpirytd');
 
+  amounttd.textContent = offerAmount.value;
+  conveyancetd.textContent = conveyance.value;
+  creditCardtd.textContent = creditCardNumber.value;
+  ccExpirytd.textContent = expiry.value;
 }
