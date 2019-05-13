e97c6a4d8c08b35b925ebc86ca22e79dbc8b8083
thorbreki1199 <40344038+thorbreki@users.noreply.github.com>
2019-05-13 12:57:07 +0000

    This is Git!

---
+++ castleapartments/static/castleapartments/js/offer.js
@@ -0,0 +1,14 @@
+const theButton = document.getElementById('reviewnextlink');
+theButton.addEventListener('click', showConfirm);
+
+const offerAmount = document.getElementById('id_offer_amount');
+const conveyance = document.getElementById('id_conveyance_date');
+const creditCardNumber = document.getElementById('id_credit_card_number');
+
+
+/**
+ * This is a useless comment because I'm too lazy to do something with it
+ */
+function showConfirm() {
+
+}
