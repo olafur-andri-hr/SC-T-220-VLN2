13e7da330512d2838649687db18c66b0633dc33e
thorbreki1199 <40344038+thorbreki@users.noreply.github.com>
2019-05-14 11:14:26 +0000

    aha! nei bíddu...

--- castleapartments/static/castleapartments/js/offer.js
+++ castleapartments/static/castleapartments/js/offer.js
@@ -2,22 +2,31 @@ const theButton = document.getElementById('paymentnextlink');
 theButton.addEventListener('click', showConfirm);
 
 const offerAmount = document.getElementById('id_offer_amount');
-const conveyance = document.getElementById('id_conveyance_date');
+const conveyanceMonth = document.getElementById('id_conveyance_date_month');
+const conveyanceDay = document.getElementById('id_conveyance_date_day');
+const conveyanceYear = document.getElementById('id_conveyance_date_year');
 const creditCardNumber = document.getElementById('id_credit_card_number');
-const expiry = document.getElementById('id_credit_card_expiration_date');
+const expiryMonth = document.getElementById(
+    'id_credit_card_expiration_date_month');
+const expiryDay = document.getElementById('id_credit_card_expiration_date_day');
+const expiryYear = document.getElementById(
+    'id_credit_card_expiration_date_year');
 
 
 /**
  * This is a useless comment because I'm too lazy to do something with it
  */
 function showConfirm() {
+  console.log('You absolute wanker!');
   const amounttd = document.getElementById('amounttd');
   const conveyancetd = document.getElementById('conveyancetd');
   const creditCardtd = document.getElementById('creditcardtd');
   const ccExpirytd = document.getElementById('ccexpirytd');
 
   amounttd.textContent = offerAmount.value;
-  conveyancetd.textContent = conveyance.value;
+  conveyancetd.textContent = conveyanceMonth.value.toString() + '/'
+    + conveyanceDay.value.toString() + '/' + conveyanceYear.value.toString();
   creditCardtd.textContent = creditCardNumber.value;
-  ccExpirytd.textContent = expiry.value;
+  ccExpirytd.textContent = expiryMonth.value.toString() + '/' +
+    expiryDay.value.toString() + '/' + expiryYear.value.toString();
 }
