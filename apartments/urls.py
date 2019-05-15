af679d546ed59bb5026e5146140edc46b2eeeb11
thorbreki1199 <40344038+thorbreki@users.noreply.github.com>
2019-05-15 15:47:16 +0000

    changing titles

--- apartments/urls.py
+++ apartments/urls.py
@@ -3,30 +3,34 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<uuid:listing_id>/', views.listing, name="listing"),
-    path('<uuid:listing_id>/offer/', views.newOffer, name='newOffer'),
-    path('<uuid:listing_id>/offers/<int:offer_id>/', views.offer),
+    path('<uuid:listing_id>/', views.listing, name="Listing"),
+    path('<uuid:listing_id>/offer/', views.newOffer, name='New offer'),
+    path('<uuid:listing_id>/offers/<int:offer_id>/', views.offer, name='Offer'),
     path(
         '<uuid:listing_id>/delete/',
         views.delete_listing,
-        name='delete_listing'
+        name='Delete listing'
     ),
     path(
         '<uuid:listing_id>/offers/<int:offer_id>/accept/',
-        views.accept_offer
+        views.accept_offer,
+        name='Accept offer'
     ),
     path(
         '<uuid:listing_id>/offers/<int:offer_id>/decline/',
-        views.decline_offer
+        views.decline_offer,
+        name='Decline offer'
     ),
     path(
         '<uuid:listing_id>/list/',
-        views.list_listing
+        views.list_listing,
+        name='New listing'
     ),
     path(
         '<uuid:listing_id>/unlist/',
-        views.unlist_listing
+        views.unlist_listing,
+        name='Delete listing'
     ),
-    path('api/search/', views.search),
-    path('api/search/<str:listing_ids>/', views.get_many_by_id)
+    path('api/search/', views.search, name='Search'),
+    path('api/search/<str:listing_ids>/', views.get_many_by_id, name="Search by")
 ]
